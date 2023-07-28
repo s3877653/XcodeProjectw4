0d7cbfc51c03ae118e7a7242179483ac1b36ee52
khoi <s3877653@rmit.edu.vn>
2023-07-28 14:40:17 +0700

    Initial Commit

---
+++ w4Product/w4ProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  w4ProductApp.swift
+//  w4Product
+//
+//  Created by Khoi Ho Tran Minh on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct w4ProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
